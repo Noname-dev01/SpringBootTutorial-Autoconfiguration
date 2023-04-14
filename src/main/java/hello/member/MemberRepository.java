d5ccd0d1c9b7b84f03d0d3cfa7a3d98e2de037e8
noname_dev <noname_dev@naver.com>
2023-04-14 19:39:47 +0900

    예제 만들기

---
+++ src/main/java/hello/member/MemberRepository.java
@@ -0,0 +1,41 @@
+package hello.member;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    private final JdbcTemplate template;
+
+    public MemberRepository(JdbcTemplate template) {
+        this.template = template;
+    }
+
+    /**
+     * 예제여서 여기서 테이블 생성 원래는 이렇게 안함
+     */
+    public void initTable(){
+        template.execute("create table member(member_id varchar primary key, name varchar)");
+    }
+
+    public void save(Member member){
+        template.update("insert into member(member_id, name) values (?,?)",
+                member.getMemberId(),
+                member.getName());
+    }
+
+    public Member find(String memberId){
+        return template.queryForObject("select member_id, name from member where member_id=?",
+                BeanPropertyRowMapper.newInstance(Member.class),
+                memberId);
+    }
+
+    public List<Member> findAll(){
+        return template.query("select member_id, name from member",
+                BeanPropertyRowMapper.newInstance(Member.class));
+    }
+}
